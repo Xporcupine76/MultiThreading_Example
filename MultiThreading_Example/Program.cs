4e6a22397c6f2963d14497360a9d93912f8e89d7
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-08 11:53:16 +0200

    Add project files.

---
+++ MultiThreading_Example/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MultiThreading_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Thread t = new Thread(WriteX);
+                t.Start();
+
+                Console.WriteLine("O");
+
+            }
+        }
+        static void WriteX()
+        {
+
+            for(int i =0; i < 10; i++)
+            {
+                Console.WriteLine(".");
+            }
+        }
+    }
+}
